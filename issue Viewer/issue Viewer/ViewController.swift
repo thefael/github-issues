d15b549737268742f81506c7697314c91c817136
Rafael Rodrigues <rafael.rodrigues@picpay.com>
2022-10-31 23:02:35 -0300

    Remove storyboard from project

--- issue Viewer/issue Viewer/ViewController.swift
+++ issue Viewer/issue Viewer/ViewController.swift
@@ -1,19 +1,8 @@
-//
-//  ViewController.swift
-//  issue Viewer
-//
-//  Created by Rafael Rodrigues on 31/10/22.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
-
